6aba783fc9d6a06e7d7e3bfb042372dad26cfe8b
tanguy.ducrocq <tanguy.ducrocq@epita.fr>
2024-07-17 17:02:43 +0200

    fix env variable

--- src/database.ts
+++ src/database.ts
@@ -6,11 +6,11 @@ dotenv.config();
 
 const AppDataSource = new DataSource({
     type: "mysql",
-    host: dotenv.MYSQL_HOST || 'localhost',
-    port: dotenv.MYSQL_PORT || 3307,
-    username: dotenv.MYSQL_USER,
-    password: dotenv.MYSQL_PASSWORD,
-    database: dotenv.MYSQL_DATABASE,
+    host: process.env.MYSQL_HOST || 'localhost',
+    port: parseInt(process.env.MYSQL_PORT || '3307', 10),
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
     entities: [Contact],
     synchronize: true,
 });
