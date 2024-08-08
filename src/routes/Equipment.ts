983f2162edabf31feb2a630493e158f1903cbc97
tanguy.ducrocq <tanguy.ducrocq@epita.fr>
2024-08-08 10:16:36 +0200

    fix(equipment): get request respond the photos too now

--- src/routes/Equipment.ts
+++ src/routes/Equipment.ts
@@ -7,9 +7,13 @@ import Equipment from "../entity/Equipment";
 const router = Router();
 
 router.get("/equipment", async (req, res) => {
-    const equipmentRepository = AppDataSource.getRepository(Equipment);
-    const equipment = await equipmentRepository.find();
-    res.json(equipment);
+    try {
+        const equipmentRepository = AppDataSource.getRepository(Equipment);
+        const equipment = await equipmentRepository.find({relations: ["photos"]});
+        res.json(equipment);
+    } catch (error) {
+        res.status(500).json({error: "An error occurred while fetching equipment."});
+    }
 });
 
 export default router;
